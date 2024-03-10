a86ae96cfd8b12a632b855630d616862aa83829a
BastienMall <bastien.mall@hotmail.fr>
2024-03-10 23:59:14 +0100

    Page Botaniste fonctionnel mais sans aucun style

--- src/Botaniste/BotanisteTabs.js
+++ src/Botaniste/BotanisteTabs.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
 import { NavigationContainer } from "@react-navigation/native";
 import { PlantSOSSVG } from "../../assets/iconesTabs/PlantSOS";
 import PlantSOSBotaniste from "../PlantSOSBotaniste/PlantSOSBotaniste";
 import { colors } from "../colors";
 import { StyleSheet, View } from "react-native";
+import { plantSOSRaw } from "../data";
 
 const BotanisteTabs = () => {
   const Tab = createMaterialBottomTabNavigator();
+
+
+  const [plantSOSListe , setPlantSOSListe ] = useState(plantSOSRaw);
 
   return (
     <View style={{ backgroundColor: colors.background, flex: 1, marginBottom: -20 }}>
@@ -26,7 +30,7 @@ const BotanisteTabs = () => {
               ),
             }}
           >
-            {() => <PlantSOSBotaniste />}
+            {() => <PlantSOSBotaniste {...{plantSOSListe, setPlantSOSListe}} />}
           </Tab.Screen>
         </Tab.Navigator>
       </NavigationContainer>
--- src/PlantSOSBotaniste/PlantSOSBotaniste.js
+++ src/PlantSOSBotaniste/PlantSOSBotaniste.js
@@ -1,12 +1,69 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text , StyleSheet, View, FlatList} from "react-native";
+import { Card , Searchbar} from "react-native-paper";
+import ModalSOS from "./ModalSOS";
 
-const PlantSOS = () => {
+const PlantSOS = (props) => {
+  const {plantSOSListe } = props;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredPlants, setFilteredPlants] = useState([]);
+  const [visible, setVisible] = useState(false);
+
+
+  useEffect(() => {
+    // eslint-disable-next-line no-shadow
+    const filteredPlants = plantSOSListe.filter(plantSOS =>
+      plantSOS.variety.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredPlants(filteredPlants);
+  }, [searchQuery, plantSOSListe]);
+
   return (
-    <Text>
-      PlantSOS
-    </Text>
+    <View>
+
+      <Text style={styles.header}>PlantSOS</Text>
+      <Searchbar
+        placeholder="Rechercher une plante"
+        onChangeText={setSearchQuery}
+        value={searchQuery}
+      />
+      <FlatList
+        data={filteredPlants}
+        renderItem={({ item }) => (
+          
+          
+          <Card key={item.index} onPress={() => setVisible(true)}>
+            <Card.Cover source={{ uri: item.url}}/>
+            <Card.Content >
+              <Text>{item.variety}</Text>
+              <Text>{item.description}</Text>
+                 
+              <Text>{item.treated ? "En cours" : "Terminer"}</Text>
+                  
+            </Card.Content>
+            
+          </Card>
+           
+         
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+
+      
+      <ModalSOS {...props} {...{ setVisible, visible }} />
+    </View>
+    
+
   );
 };
+
+
+const styles = StyleSheet.create({
+  header : {
+    fontSize : 20 , 
+    fontWeight : "bold"
+  }
+
+});
 
 export default PlantSOS;
